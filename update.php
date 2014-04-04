faa26ae83ec34d5443e267f65d47aa4f6f427a1a
gmlinscott <glinscott@uky.edu>
2014-04-03 20:55:01 -0400

    update script
    added simple update script to see when the database was last updated

---
+++ update.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+<title>Last Updated</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+<body>
+<?php echo date("D M d, Y G:i a"); ?>
+</body>
+</html>
