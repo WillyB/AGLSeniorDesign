c0a3d370b22ba356e16265630d6c1c8d11630d6b
gmlinscott <glinscott@uky.edu>
2014-04-03 21:02:58 -0400

    Revert "update script"
    This reverts commit faa26ae83ec34d5443e267f65d47aa4f6f427a1a.

--- update.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-<title>Last Updated</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-</head>
-<body>
-<?php echo date("D M d, Y G:i a"); ?>
-</body>
-</html>
