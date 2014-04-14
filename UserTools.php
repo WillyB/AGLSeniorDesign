a8c58dc0c8166dc2fdecc049980eda07c940d9aa
nastiakazadi <nastiakazadi@mail.ru>
2014-04-13 23:12:44 -0400

    UserTools Updated
    fixed redirection

--- UserTools.php
+++ UserTools.php
@@ -2,7 +2,6 @@
 <head>
 <title>AGL: User Home</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-
 <?php
 $role = $_COOKIE['role'];
 $email = $_COOKIE['email'];
@@ -10,8 +9,9 @@ $password = $_COOKIE['password'];
 
 	if (isset($_POST['viewprofile'])) 
 	{
-		header("Location: ViewProfile.php");//DO WE HAVe SAME PHP FOR VIEW AND EDIT PROFILE???
-		exit;
+		echo "<script type='text/javascript'>
+	          window.location = 'ViewProfile.php';</script>";
+		exit;	
 	}
 	//remove cookies and redirect to login.php when this button is clicked
 	if (isset($_POST['logout'])) 
@@ -32,14 +32,16 @@ $password = $_COOKIE['password'];
 	//redirect to viewshows.php when this button is clicked
 	if (isset($_POST['viewshows'])) 
 	{
-		header("Location: ViewShows.php");
+		echo "<script type='text/javascript'>
+	          window.location = 'ViewShows.php';</script>";
 		exit;	
 	}
 	//redirect to usertools.php when this button is clicked
 	if (isset($_POST['home'])) 
 	{
-		header("Location: UserTools.php");
-		exit;	
+		echo "<script type='text/javascript'>
+	          window.location = 'UserTools.php';</script>";
+		exit;
 	}
 ?>
 
@@ -47,7 +49,6 @@ $password = $_COOKIE['password'];
 <body bgcolor="#00000" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
 <!-- Save for Web Slices (UserTools.psd) -->
 <form name="form" method="post" action="UserTools.php">
-
 <table width="1401" height="967" border="0" align="center" cellpadding="0" cellspacing="0" id="Table_01">
 	<tr>
 		<td colspan="7">
@@ -58,9 +59,7 @@ $password = $_COOKIE['password'];
 	<tr>
 		<td colspan="5" rowspan="3">
 			<img src="Assets/UserTools_02.gif" width="1211" height="272" alt=""></td>
-
 		<td><input type="image" name="home" value="home" src="Assets/UserTools_03.gif"></td>
-
 		<td rowspan="5">
 			<img src="Assets/UserTools_04.gif" width="83" height="931" alt=""></td>
 		<td>
@@ -68,7 +67,6 @@ $password = $_COOKIE['password'];
 	</tr>
 	<tr>
 		<td><input type="image" name="logout" value="logout" src="Assets/UserTools_05.gif"></td>
-
 		<td>
 			<img src="Assets/spacer.gif" width="1" height="35" alt=""></td>
 	</tr>
@@ -81,12 +79,10 @@ $password = $_COOKIE['password'];
 	<tr>
 		<td rowspan="2">
 			<img src="Assets/UserTools_07.gif" width="381" height="659" alt=""></td>
-
 		<td><input type="image" name="viewprofile" value="viewprofile" src="Assets/UserTools_08.gif"></td>
 		<td rowspan="2">
 			<img src="Assets/UserTools_09.gif" width="173" height="659" alt=""></td>
 		<td><input type="image" name="viewshows" value="viewshows" src="Assets/UserTools_10.gif"></td>
-
 		<td rowspan="2">
 			<img src="Assets/UserTools_11.gif" width="162" height="659" alt=""></td>
 		<td>
@@ -102,8 +98,6 @@ $password = $_COOKIE['password'];
 	</tr>
 </table>
 <!-- End Save for Web Slices -->
-
 </form>
-
 </body>
 </html>
