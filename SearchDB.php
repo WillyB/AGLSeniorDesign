c5fd0d9b5e5fecd4397ca5c6605377582284b1b2
nastiakazadi <nastiakazadi@mail.ru>
2014-04-13 22:05:59 -0400

    SearchDB Working?
    works on localhost....

--- SearchDB.php
+++ SearchDB.php
@@ -2,9 +2,168 @@
 <head>
 <title>AGL: Search Database</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+<?php
+if (isset($_POST['Search'])) 
+{
+	$firstname = $_POST['firstname'];
+	$lastname = $_POST['lastname'];
+	$heightf = $_POST['heightf'];
+	$heightt = $_POST['heightt'];
+	$weightf = $_POST['weightf'];
+	$weightt = $_POST['weightt'];
+	$agef = $_POST['agef'];
+	$aget = $_POST['aget'];
+	$haircolor = $_POST['haircolor'];
+	$hairstyle = $_POST['hairstyle'];
+	$eyecolor = $_POST['eyecolor'];
+	$ethnicity = $_POST['ethnicity'];
+	$gender = $_POST['gender'];
+	
+	if($firstname == "" && $lastname == "" && $heightf == "" && $heightt == "" && 
+       $weightf == "" && $weightt == "" && $agef == "" && $aget == "" &&	
+	   $haircolor == "" && $hairstyle == "" && $eyecolor == "" && $ethnicity == "" &&
+	   $gender == "")
+	{
+		//user has to choose at least one parameter of the search
+		echo "<script type='text/javascript'>
+			 alert('Please, select parameters for search');".
+			 "window.location = 'SearchDB.php';</script>";//redirect back to search page
+		exit;//exit, so that the following code is not executed
+	}
+	else
+	{
+		//data to login into mysql server on multilab machine
+		$user_name = 'actorsgu_data';
+		$pass_word = 'cliffy36&winepress';
+		$database = 'actorsgu_data';
+		//$server = 'box293.bluehost.com:3306';
+		$server = 'localhost:3306';
+
+		$db_handle = mysql_connect($server, $user_name, $pass_word);
+		$db_found = mysql_select_db($database, $db_handle);
+
+		if ($db_found) 
+		{
+			//this function is used to escape any dangerous strings (SQL injections)
+			$firstname = mysql_real_escape_string($firstname, $db_handle);
+			$lastname = mysql_real_escape_string($lastname, $db_handle);
+			$heightf = mysql_real_escape_string($heightf, $db_handle); 
+			$heightt = mysql_real_escape_string($heightt, $db_handle); 
+			$weightf = mysql_real_escape_string($weightf, $db_handle); 
+			$weightt = mysql_real_escape_string($weightt, $db_handle); 
+			$agef = mysql_real_escape_string($agef, $db_handle); 
+			$aget = mysql_real_escape_string($aget, $db_handle); 
+			$haircolor = mysql_real_escape_string($haircolor, $db_handle); 
+			$hairstyle = mysql_real_escape_string($hairstyle, $db_handle); 
+			$eyecolor = mysql_real_escape_string($eyecolor, $db_handle); 
+			$ethnicity = mysql_real_escape_string($ethnicity, $db_handle); 
+			$gender = mysql_real_escape_string($gender, $db_handle); 	
+		
+			$query = 'SELECT * FROM Personnel WHERE ';
+			if($firstname!="")
+				{
+					$query.= 'First_Name="'.$firstname.'" AND ';
+				}    
+			if($lastname!="")
+			{       
+					$query.= 'Last_Name="'.$lastname.'" AND ';
+			}
+			if($haircolor!="")
+			{       
+					$query.= 'Hair_Color="'.$haircolor.'" AND ';
+			}		 
+			if($hairstyle!="")
+			{       
+					$query.= 'Hair_Style="'.$hairstyle.'" AND ';
+			}		 
+			if($eyecolor!="")
+			{       
+					$query.= 'Eye_Color="'.$eyecolor.'" AND ';
+			}		 
+			if($ethnicity!="")
+			{       
+					$query.= 'Ethnicity="'.$ethnicity.'" AND ';
+			}		 
+			if($gender!="")
+			{       
+					$query.= 'Gender="'.$gender.'" AND ';
+			}		 
+			if($heightf !="" && $heightt !="")
+			{       
+					$query.= 'Height BETWEEN' .$heightf. 'AND' .$heightt. 'AND';
+			}		 
+			if($weightf !="" && $weightt !="")
+			{       
+					$query.= 'Height BETWEEN' .$weightf. 'AND' .$weightt. 'AND';
+			}	
+			if($agef !="" && $aget !="")
+			{       
+					$query.= 'Height BETWEEN' .$agef. 'AND' .$aget. 'AND';
+			}	
+			
+			$result = substr($query,0,-5);
+			$result.=' ;';
+			
+			echo $result;
+			
+			//query the DB with received attributes
+			$final = mysql_query($result);
+			$num_rows = mysql_num_rows($final);
+			
+			//print out results if query returned result
+			if($num_rows > 0)
+			{
+				echo "<body bgcolor='silver'>";
+				echo "<h2>Search Results:</h2>";
+				echo "
+				<table border='1'>
+					<tr>
+					<th>first name</th>
+					<th>last name</th>
+					<th>email</th>
+					<th>phone</th>
+					<th>height</th>
+					<th>weight</th>
+					<th>age</th>
+					</tr>";
+					while($row = mysql_fetch_array($final))
+					{
+						echo "<tr><td>".$row['First_Name']."</td><td>".
+										$row['Last_Name']."</td><td>".
+										$row['Contact_Email']."</td><td>".
+										$row['Contact_Phone']."</td><td>".
+										$row['Height']."</td><td>".
+										$row['Weight']."</td><td>".
+										$row['Age']."</td></tr>";
+					}
+				echo "</table>";
+				echo "<br>"."<br>";
+					
+			}
+			else//if there is no specified user found in DB
+			{							
+				echo "<script type='text/javascript'>
+					 alert('NO ACTOR FOUND. Try choosing different search criteria');".
+					 "window.location = 'SearchDB.php';</script>";//redirect back to SearchDB.php page 
+				exit;			
+			}
+		}//end of if DB was found
+		else//if DB was not found
+		{
+			echo '<script type="text/javascript"> 
+				  alert("Database is not found");
+				  </script>';				  
+		}	
+		mysql_close($db_handle);
+	}//end of if at least one search parameter was received
+}//end of if search button was clicked	
+?>
+
 </head>
 <body bgcolor="#00000" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
 <!-- Save for Web Slices (SearchDB_Layout.psd) -->
+<form name="form" method="post" action="SearchDB.php">
 <table width="1401" height="968" border="0" align="center" cellpadding="0" cellspacing="0" id="Table_01">
 	<tr>
 		<td colspan="19">
@@ -15,14 +174,16 @@
 	<tr>
 		<td colspan="17" rowspan="3">
 			<img src="Assets/SearchDB_02.gif" width="1211" height="238" alt=""></td>
-		<td><input type="image" name="home" id="home" src="Assets/SearchDB_03.gif"></td>
+		<td>
+			<img src="Assets/SearchDB_03.gif" width="106" height="35" alt=""></td>
 		<td rowspan="13">
 			<img src="Assets/SearchDB_04.gif" width="83" height="897" alt=""></td>
 		<td>
 			<img src="Assets/spacer.gif" width="1" height="35" alt=""></td>
 	</tr>
 	<tr>
-		<td><input type="image" name="logout" id="logout" src="Assets/SearchDB_05.gif"></td>
+		<td>
+			<img src="Assets/SearchDB_05.gif" width="106" height="32" alt=""></td>
 		<td>
 			<img src="Assets/spacer.gif" width="1" height="32" alt=""></td>
 	</tr>
@@ -103,34 +264,34 @@
         <label for="haircolor"></label>
         <select name="haircolor" id="haircolor">
           <option selected> </option>
-          <option value="blonde">Blonde</option>
-          <option value="brown">Brown</option>
-          <option value="red">Red</option>
-          <option value="black">Black</option>
-          <option value="gray">Gray</option>
-          <option value="other">Other</option>
+          <option id="blonde">Blonde</option>
+          <option id="brown">Brown</option>
+          <option id="red">Red</option>
+          <option id="black">Black</option>
+          <option id="gray">Gray</option>
+          <option id="other">Other</option>
         </select>
         </td>
 		<td width="147" height="42" colspan="5" background="Assets/SearchDB_25.gif">&nbsp;
         <label for="hairstyle"></label>
         <select name="hairstyle" id="hairstyle">
           <option selected> </option>
-          <option value="long">Long</option>
-          <option value="short">Short</option>
-          <option value="buzz">Buzz</option>
-          <option value="blad">Bald</option>
-          <option value="other">Other</option>
+          <option id="long">Long</option>
+          <option id="short">Short</option>
+          <option id="buzz">Buzz</option>
+          <option id="blad">Bald</option>
+          <option id="other">Other</option>
         </select>
         </td>
 		<td width="146" height="42" colspan="2" background="Assets/SearchDB_26.gif">&nbsp;
         <label for="eyecolor"></label>
 		  <select name="eyecolor" id="eyecolor">
 		    <option selected> </option>
-		    <option value="blue">Blue</option>
-		    <option value="brown">Brown</option>
-		    <option value="green">Green</option>
-		    <option value="gray">Gray</option>
-		    <option value="other">Other</option>
+		    <option id="blue">Blue</option>
+		    <option id="brown">Brown</option>
+		    <option id="green">Green</option>
+		    <option id="gray">Gray</option>
+		    <option id="other">Other</option>
         </select>
         </td>
 		<td>
@@ -153,15 +314,15 @@
         <label for="ethnicity"></label>
 		  <select name="ethnicity" id="ethnicity">
 		    <option selected> </option>
-		    <option value="hispanic">Hispanic/Latino</option>
-		    <option value="african american">African American</option>
-		    <option value="asian">Asian</option>
-		    <option value="oceania">Native Hawaiian/Pacific Islander</option>
-		    <option value="american indian">American Indian</option>
-		    <option value="alaskan native">Alaskan Native</option>
-		    <option value="caucasian">Caucasian</option>
-		    <option value="middle eastern">Middle Eastern</option>
-		    <option value="other">Other</option>
+		    <option id="hispanic">Hispanic/Latino</option>
+		    <option id="african american">African American</option>
+		    <option id="asian">Asian</option>
+		    <option id="oceania">Native Hawaiian/Pacific Islander</option>
+		    <option id="american indian">American Indian</option>
+		    <option id="alaskan native">Alaskan Native</option>
+		    <option id="caucasian">Caucasian</option>
+		    <option id="middle eastern">Middle Eastern</option>
+		    <option id="other">Other</option>
         </select>
         </td>
 		<td colspan="5" rowspan="2">
@@ -170,8 +331,8 @@
         <label for="gender"></label>
 		  <select name="gender" id="gender">
 		    <option> </option>
-		    <option value="male">Male</option>
-		    <option value="female">Female</option>
+		    <option id="male">Male</option>
+		    <option id="female">Female</option>
         </select>
         </td>
 		<td>
@@ -188,7 +349,8 @@
 	<tr>
 		<td colspan="4" rowspan="2">
 			<img src="Assets/SearchDB_36.gif" width="164" height="431" alt=""></td>
-		<td colspan="4"><input type="image" name="search" id="search" src="Assets/SearchDB_37.gif"></td>
+		<td colspan="4">
+			<input type="image" name="Search" value="Search" src="Assets/SearchDB_37.gif" width="119" height="42" alt=""></td>		
 		<td colspan="3" rowspan="2">
 			<img src="Assets/SearchDB_38.gif" width="50" height="431" alt=""></td>
 		<td>
@@ -243,5 +405,6 @@
 	</tr>
 </table>
 <!-- End Save for Web Slices -->
+</form>
 </body>
 </html>
