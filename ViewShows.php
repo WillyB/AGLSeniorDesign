a364de8b0eca8ce2b036610d36ac772eefc281d3
nastiakazadi <nastiakazadi@mail.ru>
2014-04-15 16:48:43 -0400

    ViewShows

--- ViewShows.php
+++ ViewShows.php
@@ -187,47 +187,47 @@ if(isset($_POST['Cast']))
 <table width="1401" height="967" border="0" align="center" cellpadding="0" cellspacing="0" id="Table_01">
 	<tr>
 		<td colspan="5">
-			<img src="images/ViewShows_01.gif" width="1400" height="70" alt=""></td>
+			<img src="Assets/ViewShows_01.gif" width="1400" height="70" alt=""></td>
 		<td>
-			<img src="images/spacer.gif" width="1" height="70" alt=""></td>
+			<img src="Assets/spacer.gif" width="1" height="70" alt=""></td>
 	</tr>
 	<tr>
 		<td colspan="3" rowspan="3">
-			<img src="images/ViewShows_02.gif" width="1211" height="185" alt=""></td>
-		<td><input type="image" name="home" id="home" src="images/ViewShows_03.gif"></td>
+			<img src="Assets/ViewShows_02.gif" width="1211" height="185" alt=""></td>
+		<td><input type="image" name="home" id="home" src="Assets/ViewShows_03.gif"></td>
 		<td rowspan="5">
-			<img src="images/ViewShows_04.gif" width="83" height="897" alt=""></td>
+			<img src="Assets/ViewShows_04.gif" width="83" height="897" alt=""></td>
 		<td>
-			<img src="images/spacer.gif" width="1" height="35" alt=""></td>
+			<img src="Assets/spacer.gif" width="1" height="35" alt=""></td>
 	</tr>
 	<tr>
-		<td><input type="image" name="logout" id="logout" src="images/ViewShows_05.gif"></td>
+		<td><input type="image" name="logout" id="logout" src="Assets/ViewShows_05.gif"></td>
 		<td>
-			<img src="images/spacer.gif" width="1" height="32" alt=""></td>
+			<img src="Assets/spacer.gif" width="1" height="32" alt=""></td>
 	</tr>
 	<tr>
 		<td rowspan="3">
-			<img src="images/ViewShows_06.gif" width="106" height="830" alt=""></td>
+			<img src="Assets/ViewShows_06.gif" width="106" height="830" alt=""></td>
 		<td>
-			<img src="images/spacer.gif" width="1" height="118" alt=""></td>
+			<img src="Assets/spacer.gif" width="1" height="118" alt=""></td>
 	</tr>
 	<tr>
 		<td rowspan="2">
-			<img src="images/ViewShows_07.gif" width="384" height="712" alt=""></td>
-		<td width="654" height="564" background="images/ViewShows_08.gif">&nbsp;
+			<img src="Assets/ViewShows_07.gif" width="384" height="712" alt=""></td>
+		<td width="654" height="564" background="Assets/ViewShows_08.gif">&nbsp;
         <label for="users"></label>
 	    <textarea name="users" id="users" cols="76" rows="33" style="color: #FFFFFF;border:none;background-color:transparent;"></textarea>
         </td>
 		<td rowspan="2">
-			<img src="images/ViewShows_09.gif" width="173" height="712" alt=""></td>
+			<img src="Assets/ViewShows_09.gif" width="173" height="712" alt=""></td>
 		<td>
-			<img src="images/spacer.gif" width="1" height="564" alt=""></td>
+			<img src="Assets/spacer.gif" width="1" height="564" alt=""></td>
 	</tr>
 	<tr>
 		<td>
-			<img src="images/ViewShows_10.gif" width="654" height="148" alt=""></td>
+			<img src="Assets/ViewShows_10.gif" width="654" height="148" alt=""></td>
 		<td>
-			<img src="images/spacer.gif" width="1" height="148" alt=""></td>
+			<img src="Assets/spacer.gif" width="1" height="148" alt=""></td>
 	</tr>
 </table>
 <!-- End Save for Web Slices -->
