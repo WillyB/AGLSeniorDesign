ad5b98a0db44f4f7684dd92c9e66119d88b04c10
nastiakazadi <nastiakazadi@mail.ru>
2014-04-15 16:07:58 -0400

    rrrrrr

--- SearchDB.php
+++ SearchDB.php
@@ -201,15 +201,19 @@ if (isset($_POST['Search']))
 	</tr>
 	<tr>
 		<td colspan="17" rowspan="3">
-			<img src="Assests/SearchDB_02.gif" width="1211" height="238" alt=""></td>
-		<td><input type="image" name="home" value="home" src="Assets/SearchDB_03.gif"></td>
+			<img src="Assets/SearchDB_02.gif" width="1211" height="238" alt=""></td>
+		<td>
+			<td><input type="image" name="home" value="home" src="Assets/SearchDB_03.gif"></td>
 		<td rowspan="13">
 			<img src="Assets/SearchDB_04.gif" width="83" height="897" alt=""></td>
 		<td>
 			<img src="Assets/spacer.gif" width="1" height="35" alt=""></td>
 	</tr>
+
 	<tr>
-		<td><input type="image" name="logout" value="logout" src="Assets/SearchDB_05.gif"></td>
+		<td>
+		    <td><input type="image" name="logout" value="logout" src="Assets/SearchDB_05.gif"></td>
+
 		<td>
 			<img src="Assets/spacer.gif" width="1" height="32" alt=""></td>
 	</tr>
