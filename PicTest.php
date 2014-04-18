a0021f5f6ec3bda3c272f1519e30ef3b9ce9dfd9
gmlinscott <glinscott@uky.edu>
2014-04-17 20:01:20 -0400

    test upload pic v5

--- PicTest.php
+++ PicTest.php
@@ -2,7 +2,7 @@
 // max file size (Kb)
 define ("MAX_SIZE","100"); 
  
-// reads file extension
+// read file extension
 function getExtension($str) {
          $i = strrpos($str,".");
          if (!$i) { return ""; }
@@ -12,56 +12,57 @@ function getExtension($str) {
  }
  
 // error flag
- $errors=0;
-// checks if the form has been submitted
- if(isset($_POST['Submit'])) 
- {
-	echo "<h1> v4 test </h1>";
-    //reads the name of the file the user submitted for uploading
+$errors=0;
+echo "<h1> v5 test </h1>";
+// check if the form has been submitted
+if(isset($_POST['Submit'])) 
+{
+	//reads the name of the file the user submitted for uploading
     $image=$_FILES['image']['name'];
+	
     //if it is not empty
     if ($image) 
     {
-    //get the original name of the file from the clients machine
-        $filename = stripslashes($_FILES['image']['name']);
-    //get the extension of the file in a lower case format
-        $extension = getExtension($filename);
-        $extension = strtolower($extension);
-    //if it is not a known extension, we will suppose it is an error and will not  upload the file,  
-    //otherwise we will do more tests
-    if (($extension != "jpg") && ($extension != "jpeg") && ($extension != "png") && ($extension != "gif")) 
-        {
-        //print error message
-            echo '<h1>Unknown extension!</h1>';
-            $errors=1;
-        }
-    else
-        {
-            //get the size of the image in bytes
-            //$_FILES['image']['tmp_name'] is the temporary filename of the file
-            //in which the uploaded file was stored on the server
-            $size=filesize($_FILES['image']['tmp_name']);
- 
-            //compare the size with the maxim size we defined and print error if bigger
-            if ($size > MAX_SIZE*1024)
-            {
-                echo '<h1>You have exceeded the size limit!</h1>';
-                $errors=1;
-            }
- 
-            else{
-            //the new name will be containing the full path where will be stored (images folder)
-            $temp=resizeImage($_FILES['image']['tmp_name'],200,200);
-            $imgfile="images/".$image;
- 
-			$email = $_POST('Contact_Email');
-            imagejpeg ( $temp, $imgfile );
-			$result = "UPDATE User SET Picture = '$imgfile' WHERE Contact_Email = '$Contact_Email'";
-            }
- 
- 
-        }
-    }
+		// get the original name of the file from the clients machine
+		$filename = stripslashes($_FILES['image']['name']);
+		
+		// get file extension (lower case)
+		$extension = getExtension($filename);
+		$extension = strtolower($extension);
+		
+		// ensure known file extension
+		if (($extension != "jpg") && ($extension != "jpeg") && ($extension != "png") && ($extension != "gif")) 
+			{
+			//print error message
+				echo '<h1>Unknown extension!</h1>';
+				$errors=1;
+			}
+		else
+			{
+				//get the size of the image in bytes
+				//$_FILES['image']['tmp_name'] is the temporary filename of the file
+				//in which the uploaded file was stored on the server
+				$size=filesize($_FILES['image']['tmp_name']);
+	 
+				//compare the size with the maxim size we defined and print error if bigger
+				if ($size > MAX_SIZE*1024)
+				{
+					echo '<h1>You have exceeded the size limit!</h1>';
+					$errors=1;
+				}
+	 
+				else{
+				//the new name will be containing the full path where will be stored (images folder)
+				$temp=resizeImage($_FILES['image']['tmp_name'],200,200);
+				$imgfile="images/".$image;
+	 
+				$email = $_POST('Contact_Email');
+				imagejpeg ( $temp, $imgfile );
+				echo "<h1>1/2</h2>";
+				$result = "UPDATE Personnel SET Picture = '$imgfile' WHERE Contact_Email = '$Contact_Email'";
+				}
+			}
+		}
  
     else
     {
