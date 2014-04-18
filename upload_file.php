563b1bb9173260b27324402449b652a3dff2a799
gmlinscott <glinscott@uky.edu>
2014-04-17 22:31:06 -0400

    upload file test v4

--- upload_file.php
+++ upload_file.php
@@ -10,39 +10,37 @@ if ((($_FILES["file"]["type"] == "image/gif")
 || ($_FILES["file"]["type"] == "image/png"))
 && ($_FILES["file"]["size"] < 20000)
 && in_array($extension, $allowedExts))
-  {
-  if ($_FILES["file"]["error"] > 0)
-    {
-    echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
-    }
-  else
-    {
-    echo "Upload: " . $_FILES["file"]["name"] . "<br>";
-    echo "Type: " . $_FILES["file"]["type"] . "<br>";
-    echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-    echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
+{
+	if ($_FILES["file"]["error"] > 0)
+	{
+		echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+	}
+	else
+	{
+		echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+		echo "Type: " . $_FILES["file"]["type"] . "<br>";
+		echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+		echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
 
-    if (file_exists("upload/" . $_FILES["file"]["name"]))
-      {
-      echo $_FILES["file"]["name"] . " already exists. ";
-      }
-    else
-      {
-		$tmpFile = $_FILES["file"]["tmp_name"];
-		list($width, $height) = getimagesize($tmpFile);
-		
-	if ($width >= 400 && $height >= 400) {
-        $image = new Imagick($tmpFile);
-        $image->thumbnailImage(400, 400);
-        $image->writeImage($fileName);
-    }
-	
-        move_uploaded_file($tmpFile, 
-      "upload/" . $_FILES["file"]["name"]);
-      echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
+		if (file_exists("upload/" . $_FILES["file"]["name"]))
+		{
+			echo $_FILES["file"]["name"] . " already exists. ";
+		}
+		else
+		{
+			$tmpFile = $_FILES["file"]["tmp_name"];
+			list($width, $height) = getimagesize($tmpFile);
 
-    }
-  }
+			if ($width >= 400 && $height >= 400) {
+				$image = new Imagick($tmpFile);
+				$image->thumbnailImage(400, 400);
+				$image->writeImage($fileName);
+			}
+			move_uploaded_file($tmpFile, "upload/" . $_FILES["file"]["name"]);
+			echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
+		}
+	}
+}
 else
   {
   echo "Invalid file";
