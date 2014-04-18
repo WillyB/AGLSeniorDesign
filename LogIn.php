cc7ed30b2fa6832981063f28c634694be0cb5a18
OmarAlzayed <onal222@g.uky.edu>
2014-04-17 21:56:15 -0400

    Update LogIn.php

--- LogIn.php
+++ LogIn.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-<title>AGL Actors DB</title>
+<title>AGL: Log In</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <?php
 $db_field = "";
@@ -8,9 +8,8 @@ $role = "";
 $email = "";
 $Password = "";
 $email2 = "";
-$Password2 = "";
 //RETURNING USER LOGIN PROCEDURE
-	if (isset($_POST['LogIn'])) 
+	if (isset($_POST['login'])) 
 	{
 		$email = $_POST['email'];
 		$Password = $_POST['Password'];
@@ -76,9 +75,9 @@ $Password2 = "";
 						case 0://admin login
 						
 							   //save role, email, and password in a cookie
-							    setCookie('role', $role);
-								setCookie('email',$email);
-								setCookie('password',$Password);//delete later
+							    setcookie('role', $role);
+								setcookie('email',$email);
+								setcookie('password',$Password);//delete later
 						        //setCookie('password',$hashedPassword);//uncomment later
 								
 								echo "<script type='text/javascript'>
@@ -88,9 +87,9 @@ $Password2 = "";
 								
 						case 1://director login
 							   //save email and password in a cookie
-							    setCookie('role', $role);
-								setCookie('email',$email);
-								setCookie('password',$Password);//delete later
+							    setcookie('role', $role);
+								setcookie('email',$email);
+								setcookie('password',$Password);//delete later
 						        //setCookie('password',$hashedPassword); uncomment later
 								
 								echo "<script type='text/javascript'>
@@ -101,9 +100,9 @@ $Password2 = "";
 						case 2://regular user login
 						
 							   //save email and password in a cookie
-							    setCookie('role', $role);
-								setCookie('email',$email);
-								setCookie('password',$Password);//delete later
+							    setcookie('role', $role);
+								setcookie('email',$email);
+								setcookie('password',$Password);//delete later
 						        //setCookie('password',$hashedPassword); uncomment later
 								
 								echo "<script type='text/javascript'>
@@ -130,74 +129,52 @@ $Password2 = "";
 		mysql_close($db_handle);
 	}
 //NEW USER REGISTRATION PROCEDURE
-	if (isset($_POST['CreateProfile'])) 
+	if (isset($_POST['register'])) 
 	{
-		$email2 = $_POST['email2'];
-		$Password2 = $_POST['Password2'];
-		
-		if($email2 == "" && $Password2 == "")//display error message in case username and password fields are left blank
+		$email2 = $_POST['email'];
+
+		if($email2 == "")//display error message in case username and password fields are left blank
 		{
 			echo "<script type='text/javascript'>
-				 alert('Please, make sure you enter your email and password');".
+				 alert('Please make sure to enter a valid email.');".
 				 "window.location = 'LogIn.php';</script>";//redirect back to login page    
 			exit;//exit, so that the following code is not executed
 		}
 
-		if($email2 == "")//display error message in case user left username field blank
-		{
-			echo "<script type='text/javascript'>
-				 alert('Please, make sure you enter your email');".
-				 "window.location = 'LogIn.php';</script>";//redirect back to login page
-			exit;//exit, so that the following code is not executed
-		}
-		
-		if($Password2 == "")//display error message in case user left password field blank
-		{
-			echo "<script type='text/javascript'>
-				 alert('Please, make sure you enter your password');".
-				 "window.location = 'LogIn.php';</script>";//redirect back to login page   
-			exit;//exit, so that the following code is not executed
-		}
-		
 		//data to login into mysql server on multilab machine
 		$user_name = 'actorsgu_data';
 		$pass_word = 'cliffy36&winepress';
 		$database = 'actorsgu_data';
-		//$server = 'box293.bluehost.com:3306';
-		$server = 'localhost:3306';
+		$server = 'localhost:3306';//change back to 'localhost:3306';
 
 		$db_handle = mysql_connect($server, $user_name, $pass_word);
 		$db_found = mysql_select_db($database, $db_handle);
-		
+
 		if ($db_found) 
 		{
 			//this function is used to escape any dangerous strings (SQL injections)
-			$email2 = mysql_real_escape_string($email2, $db_handle);
-			$Password2 = mysql_real_escape_string($Password2, $db_handle);	
-			//$hashedPassword2 = password_hash($pass, PASSWORD_BCRYPT);	uncomment later		
+			$email2 = mysql_real_escape_string($email2, $db_handle);	
 
-			//check if user already exist by quering the Personnel table
+			//check if user already exist by quering the personnel table
 			//change $Password2 to $hashedPassword2 when hash function works
-			$sql2 = ("SELECT Contact_Email FROM Personnel
-										  WHERE Contact_Email = '$email2'
-											AND password = '$Password2'
-								  ");
-			$result2 = mysql_query($sql2);					 
-			
-			$num_rows2 = mysql_num_rows($result2);
-			//Even though, it is "Create Profile", still, check if user exists
-			if ($num_rows2 > 0)//if user exists in DB already, redirect back to log in page			
+			$sql = ("SELECT Contact_Email FROM Personnel WHERE Contact_Email = '$email2'");
+			$result = mysql_query($sql);					 
+
+			$num_rows = mysql_num_rows($result);
+			//Even though, it is "Register", still, check if user exists
+			if ($num_rows > 0)//if user exists in DB already, redirect back to log in page			
 			{
 				echo "<script type='text/javascript'>
-					 alert('You are an existing user, please go back and login using your email and password');".
+					 alert('You already have an existing account. Please log in instead.');".
 					 "window.location = 'LogIn.php';</script>";//redirect back to login page  					
 				exit;//stop execution if this is the case				
 			}
 			else//if user does not exist in DB, redirect to DB 
 			{
+				setcookie('email',$email2);
 				echo "<script type='text/javascript'>
-					 alert('Please fill out the form');".
-					 "window.location = 'EditProfile.php';</script>";  	
+					 alert('Please fill out the following registration form.');".
+					 "window.location = 'Register.php';</script>";  	
 				exit;			
 			}			
 		}
@@ -217,98 +194,65 @@ $Password2 = "";
 <form name="form1" method="post" action="LogIn.php">
 <table width="1401" height="968" border="0" align="center" cellpadding="0" cellspacing="0" id="Table_01">
 	<tr>
-		<td colspan="10">
+		<td colspan="11">
 			<img src="Assets/LogIn_01.gif" width="1400" height="295" alt=""></td>
 		<td>
 			<img src="Assets/spacer.gif" width="1" height="295" alt=""></td>
 	</tr>
 	<tr>
-		<td rowspan="10">
-			<img src="Assets/LogIn_02.gif" width="209" height="672" alt=""></td>
 		<td rowspan="6">
+			<img src="Assets/LogIn_02.gif" width="209" height="672" alt=""></td>
+		<td rowspan="5">
 			<img src="Assets/LogIn_03.gif" width="340" height="170" alt=""></td>
-		<td colspan="8">
-			<img src="Assets/LogIn_04.gif" width="851" height="39" alt=""></td>
+		<td colspan="9">
+			<img src="Assets/LogIn_04.gif" width="851" height="45" alt=""></td>
 		<td>
-			<img src="Assets/spacer.gif" width="1" height="39" alt=""></td>
+			<img src="Assets/spacer.gif" width="1" height="45" alt=""></td>
 	</tr>
 	<tr>
-		<td rowspan="9">
-			<img src="Assets/LogIn_05.gif" width="185" height="633" alt=""></td>
-	  <td width="174" height="51" colspan="2" rowspan="3" background="LogIn_06.gif">&nbsp;
-	    <label for="email"></label>
-		<!--RETURNING USER LOGIN -->
-	    <input name="email" type="text" id="email" style="color: #FFFFFF;border:none;background-color:transparent;"></td>
-		<td width="178" height="51" colspan="3" rowspan="3" background="LogIn_07.gif">&nbsp;
-		  <label for="Password"></label>
-	    <input type="password" name="Password" id="Password" style="color: #FFFFFF;border:none;background-color:transparent;"></td>
-		<td colspan="2">
-			<img src="Assets/LogIn_08.gif" width="314" height="6" alt=""></td>
-		<td>
-			<img src="Assets/spacer.gif" width="1" height="6" alt=""></td>
-	</tr>
-	<tr>
-		<td>
-			<!-- CALL TO JAVASCRIPT EXECUTE() --> 
-            <input type="image" name="LogIn" value="login" src="Assets/LogIn_09.gif" width="66" height="37"/></td>
-		<td rowspan="8">
+		<td rowspan="5">
+			<img src="Assets/LogIn_05.gif" width="185" height="627" alt=""></td>
+		<td width="174" height="37" colspan="2" background="Assets/LogIn_06.gif">&nbsp;</td>
+		<td rowspan="2">
+			<img src="Assets/LogIn_07.gif" width="5" height="81" alt=""></td>
+		<td width="173" height="37" colspan="3" background="Assets/LogIn_08.gif">&nbsp;</td>
+		<td><input type="image" name="login" value="login" src="Assets/LogIn_09.gif" id="login"></td>
+		<td rowspan="5">
 			<img src="Assets/LogIn_10.gif" width="248" height="627" alt=""></td>
 		<td>
 			<img src="Assets/spacer.gif" width="1" height="37" alt=""></td>
 	</tr>
 	<tr>
-		<td rowspan="2">
-			<img src="Assets/LogIn_11.gif" width="66" height="38" alt=""></td>
+		<td colspan="2">
+			<img src="Assets/LogIn_11.gif" width="174" height="44" alt=""></td>
+		<td colspan="4">
+			<img src="Assets/LogIn_12.gif" width="239" height="44" alt=""></td>
 		<td>
-			<img src="Assets/spacer.gif" width="1" height="8" alt=""></td>
-	</tr>
-	<tr>
-		<td colspan="5">
-			<img src="Assets/LogIn_12.gif" width="352" height="30" alt=""></td>
-		<td>
-			<img src="Assets/spacer.gif" width="1" height="30" alt=""></td>
-	</tr>
-	<tr>
-		<td rowspan="5">
-			<img src="Assets/LogIn_13.gif" width="67" height="552" alt=""></td>
-		<td width="351" height="50" colspan="5" background="LogIn_14.gif">&nbsp;
-		  <label for="email2"></label>
-	    <input type="text" name="email2" id="email2" style="color: #FFFFFF;border:none;background-color:transparent;"></td>
-		<td>
-			<img src="Assets/spacer.gif" width="1" height="50" alt=""></td>
-	</tr>
-	<tr>
-		<td rowspan="4">
-			<img src="Assets/LogIn_15.gif" width="340" height="502" alt=""></td>
-		<td width="226" height="51" colspan="2" rowspan="3" background="LogIn_16.gif">&nbsp;
-		  <label for="password2"></label>
-	    <input type="password" name="Password2" id="password2" style="color: #FFFFFF;border:none;background-color:transparent;"></td>
-		<td colspan="3">
-			<img src="Assets/LogIn_17.gif" width="125" height="6" alt=""></td>
-		<td>
-			<img src="Assets/spacer.gif" width="1" height="6" alt=""></td>
+			<img src="Assets/spacer.gif" width="1" height="44" alt=""></td>
 	</tr>
 	<tr>
 		<td rowspan="3">
-			<img src="Assets/LogIn_18.gif" width="11" height="496" alt=""></td>
-		<td colspan="2">
-			<!-- NEW USER REGISTRATION-->
-            <input type="image" name="CreateProfile" value="createprofile" src="Assets/LogIn_19.gif" width="114" height="38"/>
-            </td>
+			<img src="Assets/LogIn_13.gif" width="67" height="546" alt=""></td>
+		<td width="226" height="38" colspan="3" background="Assets/LogIn_14.gif">&nbsp;</td>
+		<td rowspan="3">
+			<img src="Assets/LogIn_15.gif" width="11" height="546" alt=""></td>
+		<td colspan="2"><input type="image" name="register" value="register" src="Assets/LogIn_16.gif" id="register"></td>
 		<td>
 			<img src="Assets/spacer.gif" width="1" height="38" alt=""></td>
 	</tr>
 	<tr>
+		<td colspan="3" rowspan="2">
+			<img src="Assets/LogIn_17.gif" width="226" height="508" alt=""></td>
 		<td colspan="2" rowspan="2">
-			<img src="Assets/LogIn_20.gif" width="114" height="458" alt=""></td>
+			<img src="Assets/LogIn_18.gif" width="114" height="508" alt=""></td>
 		<td>
-			<img src="Assets/spacer.gif" width="1" height="7" alt=""></td>
+			<img src="Assets/spacer.gif" width="1" height="6" alt=""></td>
 	</tr>
 	<tr>
-		<td colspan="2">
-			<img src="Assets/LogIn_21.gif" width="226" height="451" alt=""></td>
 		<td>
-			<img src="Assets/spacer.gif" width="1" height="451" alt=""></td>
+			<img src="Assets/LogIn_19.gif" width="340" height="502" alt=""></td>
+		<td>
+			<img src="Assets/spacer.gif" width="1" height="502" alt=""></td>
 	</tr>
 	<tr>
 		<td>
@@ -322,7 +266,9 @@ $Password2 = "";
 		<td>
 			<img src="Assets/spacer.gif" width="107" height="1" alt=""></td>
 		<td>
-			<img src="Assets/spacer.gif" width="119" height="1" alt=""></td>
+			<img src="Assets/spacer.gif" width="5" height="1" alt=""></td>
+		<td>
+			<img src="Assets/spacer.gif" width="114" height="1" alt=""></td>
 		<td>
 			<img src="Assets/spacer.gif" width="11" height="1" alt=""></td>
 		<td>
