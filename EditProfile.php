56b9ebfe058886fd934def7880f9bd5d69590a42
OmarAlzayed <onal222@g.uky.edu>
2014-04-19 00:11:49 -0400

    Update EditProfile.php

--- EditProfile.php
+++ EditProfile.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-<title>AGL: Edit Profile 12:10</title>
+<title>AGL: Edit Profile</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <?php
 	$role = $_COOKIE['role'];
@@ -311,6 +311,54 @@
 			$stateArray = array('' => '', 
 								'AL' => 'AL',
 								'AK' => 'AK',
+								'AZ' => 'AZ',
+								'AR' => 'AR',
+								'CA' => 'CA',
+								'CO' => 'CO',
+								'CT' => 'CT',
+								'DE' => 'DE',
+								'DC' => 'DC',
+								'FL' => 'FL',
+								'GA' => 'GA',
+								'HI' => 'HI',
+								'ID' => 'ID',
+								'IL' => 'IL',
+								'IN' => 'IN',
+								'IA' => 'IA',
+								'KS' => 'KS',
+								'KY' => 'KY',
+								'LA' => 'LA',
+								'ME' => 'ME',
+								'MD' => 'MD',
+								'MA' => 'MA',
+								'MI' => 'MI',
+								'MN' => 'MN',
+								'MS' => 'MS',
+								'MO' => 'MO',
+								'MT' => 'MT',
+								'NE' => 'NE',
+								'NV' => 'NV',
+								'NH' => 'NH',
+								'NJ' => 'NJ',
+								'NM' => 'NM',
+								'NY' => 'NY',
+								'NC' => 'NC',
+								'ND' => 'ND',
+								'OH' => 'OH',
+								'OK' => 'OK',
+								'OR' => 'OR',
+								'PA' => 'PA',
+								'RI' => 'RI',
+								'SC' => 'SC',
+								'SD' => 'SD',
+								'TN' => 'TN',
+								'TX' => 'TX',
+								'UT' => 'UT',
+								'VT' => 'VT',
+								'VA' => 'VA',
+								'WA' => 'WA',
+								'WV' => 'WV',
+								'WI' => 'WI',
 								'WY' => 'WY');
 			$stateUnit = $State;
 			echo '<select class="select" name="state">';
