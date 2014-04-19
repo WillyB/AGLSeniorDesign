70d0497d971d177eeabce5dbfada124c5de096e1
OmarAlzayed <onal222@g.uky.edu>
2014-04-18 23:51:01 -0400

    Update ViewShow.php

--- ViewShow.php
+++ ViewShow.php
@@ -8,113 +8,113 @@
 <table width="1401" height="880" border="0" align="center" cellpadding="0" cellspacing="0" id="Table_01">
 	<tr>
 		<td colspan="6">
-			<img src="images/ViewShow_01.gif" width="1400" height="71" alt=""></td>
+			<img src="Assets/ViewShow_01.gif" width="1400" height="71" alt=""></td>
 		<td>
-			<img src="images/spacer.gif" width="1" height="71" alt=""></td>
+			<img src="Assets/spacer.gif" width="1" height="71" alt=""></td>
 	</tr>
 	<tr>
 		<td colspan="4" rowspan="3">
-			<img src="images/ViewShow_02.gif" width="1211" height="187" alt=""></td>
+			<img src="Assets/ViewShow_02.gif" width="1211" height="187" alt=""></td>
 		<td>
 			<input type="image" name="home" value="home" src="Assets/ViewShow_03.gif" id="home"></td>
 		<td rowspan="15">
-			<img src="images/ViewShow_04.gif" width="83" height="809" alt=""></td>
+			<img src="Assets/ViewShow_04.gif" width="83" height="809" alt=""></td>
 		<td>
-			<img src="images/spacer.gif" width="1" height="33" alt=""></td>
+			<img src="Assets/spacer.gif" width="1" height="33" alt=""></td>
 	</tr>
 	<tr>
 		<td>
 			<input type="image" name="logout" value="logout" src="Assets/ViewShow_05.gif" id"logout"></td>
 		<td>
-			<img src="images/spacer.gif" width="1" height="33" alt=""></td>
+			<img src="Assets/spacer.gif" width="1" height="33" alt=""></td>
 	</tr>
 	<tr>
 		<td rowspan="13">
-			<img src="images/ViewShow_06.gif" width="106" height="743" alt=""></td>
+			<img src="Assets/ViewShow_06.gif" width="106" height="743" alt=""></td>
 		<td>
-			<img src="images/spacer.gif" width="1" height="121" alt=""></td>
+			<img src="Assets/spacer.gif" width="1" height="121" alt=""></td>
 	</tr>
 	<tr>
 		<td rowspan="12">
-			<img src="images/ViewShow_07.gif" width="396" height="622" alt=""></td>
-		<td width="676" height="245" colspan="2" background="images/ViewShow_08.gif">&nbsp;</td>
+			<img src="Assets/ViewShow_07.gif" width="396" height="622" alt=""></td>
+		<td width="676" height="245" colspan="2" background="Assets/ViewShow_08.gif">&nbsp;</td>
 		<td rowspan="12">
-			<img src="images/ViewShow_09.gif" width="139" height="622" alt=""></td>
+			<img src="Assets/ViewShow_09.gif" width="139" height="622" alt=""></td>
 		<td>
-			<img src="images/spacer.gif" width="1" height="245" alt=""></td>
+			<img src="Assets/spacer.gif" width="1" height="245" alt=""></td>
 	</tr>
 	<tr>
 		<td colspan="2">
-			<img src="images/ViewShow_10.gif" width="676" height="61" alt=""></td>
+			<img src="Assets/ViewShow_10.gif" width="676" height="61" alt=""></td>
 		<td>
-			<img src="images/spacer.gif" width="1" height="61" alt=""></td>
+			<img src="Assets/spacer.gif" width="1" height="61" alt=""></td>
 	</tr>
 	<tr>
 		<td rowspan="10">
-			<img src="images/ViewShow_11.gif" width="167" height="316" alt=""></td>
-		<td width="509" height="34" background="images/ViewShow_12.gif">&nbsp;
+			<img src="Assets/ViewShow_11.gif" width="167" height="316" alt=""></td>
+		<td width="509" height="34" background="Assets/ViewShow_12.gif">&nbsp;
         <input name="title" type="text" id="title" style="color: #FFFFFF;border:none;background-color:transparent;" size="70" readonly>
         </td>
 		<td>
-			<img src="images/spacer.gif" width="1" height="34" alt=""></td>
+			<img src="Assets/spacer.gif" width="1" height="34" alt=""></td>
 	</tr>
 	<tr>
 		<td>
-			<img src="images/ViewShow_13.gif" width="509" height="5" alt=""></td>
+			<img src="Assets/ViewShow_13.gif" width="509" height="5" alt=""></td>
 		<td>
-			<img src="images/spacer.gif" width="1" height="5" alt=""></td>
+			<img src="Assets/spacer.gif" width="1" height="5" alt=""></td>
 	</tr>
 	<tr>
-		<td width="509" height="34" background="images/ViewShow_14.gif">&nbsp;
+		<td width="509" height="34" background="Assets/ViewShow_14.gif">&nbsp;
         <input name="playwright" type="text" id="playwright" style="color: #FFFFFF;border:none;background-color:transparent;" size="70" readonly>
         </td>
 		<td>
-			<img src="images/spacer.gif" width="1" height="34" alt=""></td>
+			<img src="Assets/spacer.gif" width="1" height="34" alt=""></td>
 	</tr>
 	<tr>
 		<td>
-			<img src="images/ViewShow_15.gif" width="509" height="5" alt=""></td>
+			<img src="Assets/ViewShow_15.gif" width="509" height="5" alt=""></td>
 		<td>
-			<img src="images/spacer.gif" width="1" height="5" alt=""></td>
+			<img src="Assets/spacer.gif" width="1" height="5" alt=""></td>
 	</tr>
 	<tr>
-		<td width="509" height="34" background="images/ViewShow_16.gif">&nbsp;
+		<td width="509" height="34" background="Assets/ViewShow_16.gif">&nbsp;
         <input name="director" type="text" id="director" style="color: #FFFFFF;border:none;background-color:transparent;" size="70" readonly>
         </td>
 		<td>
-			<img src="images/spacer.gif" width="1" height="34" alt=""></td>
+			<img src="Assets/spacer.gif" width="1" height="34" alt=""></td>
 	</tr>
 	<tr>
 		<td>
-			<img src="images/ViewShow_17.gif" width="509" height="5" alt=""></td>
+			<img src="Assets/ViewShow_17.gif" width="509" height="5" alt=""></td>
 		<td>
-			<img src="images/spacer.gif" width="1" height="5" alt=""></td>
+			<img src="Assets/spacer.gif" width="1" height="5" alt=""></td>
 	</tr>
 	<tr>
-		<td width="509" height="34" background="images/ViewShow_18.gif">&nbsp;
+		<td width="509" height="34" background="Assets/ViewShow_18.gif">&nbsp;
         <input name="auditiondates" type="text" id="auditiondates" style="color: #FFFFFF;border:none;background-color:transparent;" size="70" readonly>
         </td>
 		<td>
-			<img src="images/spacer.gif" width="1" height="34" alt=""></td>
+			<img src="Assets/spacer.gif" width="1" height="34" alt=""></td>
 	</tr>
 	<tr>
 		<td>
-			<img src="images/ViewShow_19.gif" width="509" height="5" alt=""></td>
+			<img src="Assets/ViewShow_19.gif" width="509" height="5" alt=""></td>
 		<td>
-			<img src="images/spacer.gif" width="1" height="5" alt=""></td>
+			<img src="Assets/spacer.gif" width="1" height="5" alt=""></td>
 	</tr>
 	<tr>
-		<td width="509" height="34" background="images/ViewShow_20.gif">&nbsp;
+		<td width="509" height="34" background="Assets/ViewShow_20.gif">&nbsp;
         <input name="showdates" type="text" id="showdates" style="color: #FFFFFF;border:none;background-color:transparent;" size="70" readonly>
         </td>
 		<td>
-			<img src="images/spacer.gif" width="1" height="34" alt=""></td>
+			<img src="Assets/spacer.gif" width="1" height="34" alt=""></td>
 	</tr>
 	<tr>
 		<td>
-			<img src="images/ViewShow_21.gif" width="509" height="126" alt=""></td>
+			<img src="Assets/ViewShow_21.gif" width="509" height="126" alt=""></td>
 		<td>
-			<img src="images/spacer.gif" width="1" height="126" alt=""></td>
+			<img src="Assets/spacer.gif" width="1" height="126" alt=""></td>
 	</tr>
 </table>
 <!-- End Save for Web Slices -->
