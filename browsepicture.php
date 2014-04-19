63410cc0466f296369a56447d9ebd98f9e7c03cf
OmarAlzayed <onal222@g.uky.edu>
2014-04-18 23:03:15 -0400

    The Browse Picture Php

---
+++ browsepicture.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>AGL: Browse Picture</title>
+</head>
+<body bgcolor="#00000" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+<form action="upload_file.php" method="post" enctype="multipart/form-data">
+<input type="file" name="file" id="file"><br>
+<input type="submit" name="submit" value="Submit">
+</form>
+<body>
+</body>
+</html>
