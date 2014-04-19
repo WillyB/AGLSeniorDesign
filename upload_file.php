17ffa14f7d20531eaf57e0335cad280722b02cb5
gmlinscott <glinscott@uky.edu>
2014-04-18 20:47:04 -0400

    debug statements

--- upload_file.php
+++ upload_file.php
@@ -4,6 +4,7 @@ include 'MasterCode.php';
 connectToDatabase();
 if ($dbFound)
 {
+	echo "<h1>7</h1>";
 	$allowedExts = array("gif", "jpeg", "jpg", "png");
 	$temp = explode(".", $_FILES["file"]["name"]);
 	$extension = end($temp);
@@ -16,12 +17,14 @@ if ($dbFound)
 	&& ($_FILES["file"]["size"] < 200000)
 	&& in_array($extension, $allowedExts))
 	{
+		echo "<h1>20</h1>";
 		if ($_FILES["file"]["error"] > 0)
 		{
 			echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
 		}
 		else
 		{
+			echo "<h1>27</h1>";
 			echo "Upload: " . $_FILES["file"]["name"] . "<br>";
 			echo "Type: " . $_FILES["file"]["type"] . "<br>";
 			echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
@@ -33,6 +36,7 @@ if ($dbFound)
 			}
 			else
 			{
+				echo "<h1>39</h1>";
 				$tmpFile = $_FILES["file"]["tmp_name"];
 				$fileName = "upload/" . $_FILES["file"]["name"];
 				list($width, $height) = getimagesize($tmpFile);
@@ -44,11 +48,12 @@ if ($dbFound)
 				}
 				else
 				{
+					echo "<h1>51</h1>";
 					move_uploaded_file($tmpFile, $fileName);
 					//$email = $_POST('Contact_Email');
 					$email = "joss@actors.com";
 					$result1 = mysql_query($con,"UPDATE Personnel SET Picture = '" . $fileName . "' WHERE Contact_Email = '$email'");
-					
+					echo "<h1>56</h1>";
 					if (!$result1)
 						echo "<h1>Did not work</h1>";
 				}
