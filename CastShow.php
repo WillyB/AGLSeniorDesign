000e7949d40769446c31ac0fe720715237206dba
WillyB <bateswc@gmail.com>
2014-04-23 09:31:14 -0400

    Sync changes to Show pages

---
+++ CastShow.php
@@ -0,0 +1,134 @@
+<html>
+<head>
+<title>AGL: Cast Show</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+<body bgcolor="#00000" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+<!-- Save for Web Slices (CastShow.psd) -->
+<form name="form" method="post" action="CastShow.php">
+<table width="1401" height="1152" border="0" align="center" cellpadding="0" cellspacing="0" id="Table_01">
+	<tr>
+		<td colspan="11">
+			<img src="Assets/CastShow_01.gif" width="1400" height="71" alt=""></td>
+		<td>
+			<img src="Assets/spacer.gif" width="1" height="71" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="9" rowspan="3">
+			<img src="Assets/CastShow_02.gif" width="1211" height="138" alt=""></td>
+		<td>
+			<input type="image" name="home" value="home" src="Assets/CastShow_03.gif" id="home"></td>
+		<td rowspan="15">
+			<img src="Assets/CastShow_04.gif" width="83" height="1050" alt=""></td>
+		<td>
+			<img src="Assets/spacer.gif" width="1" height="33" alt=""></td>
+	</tr>
+	<tr>
+		<td>
+			<input type="image" name="logout" value="logout" src="Assets/CastShow_05.gif" id"logout"></td>
+		<td>
+			<img src="Assets/spacer.gif" width="1" height="33" alt=""></td>
+	</tr>
+	<tr>
+		<td rowspan="13">
+			<img src="Assets/CastShow_06.gif" width="106" height="984" alt=""></td>
+		<td>
+			<img src="Assets/spacer.gif" width="1" height="72" alt=""></td>
+	</tr>
+	<tr>
+		<td rowspan="12">
+			<img src="Assets/CastShow_07.gif" width="217" height="912" alt=""></td>
+		<td width="938" height="44" colspan="7" background="Assets/CastShow_08.gif">&nbsp;</td>
+		<td rowspan="12">
+			<img src="Assets/CastShow_09.gif" width="56" height="912" alt=""></td>
+		<td>
+			<img src="Assets/spacer.gif" width="1" height="44" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="7">
+			<img src="Assets/CastShow_10.gif" width="938" height="43" alt=""></td>
+		<td>
+			<img src="Assets/spacer.gif" width="1" height="43" alt=""></td>
+	</tr>
+	<tr>
+		<td width="938" height="121" colspan="7" background="Assets/CastShow_11.gif">&nbsp;</td>
+		<td>
+			<img src="Assets/spacer.gif" width="1" height="121" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="7">
+			<img src="Assets/CastShow_12.gif" width="938" height="42" alt=""></td>
+		<td>
+			<img src="Assets/spacer.gif" width="1" height="42" alt=""></td>
+	</tr>
+	<tr>
+		<td width="410" height="566" rowspan="5" background="Assets/CastShow_13.gif">&nbsp;</td>
+		<td colspan="3">
+			<img src="Assets/CastShow_14.gif" width="120" height="171" alt=""></td>
+		<td width="408" height="566" colspan="3" rowspan="5" background="Assets/CastShow_15.gif">&nbsp;</td>
+		<td>
+			<img src="Assets/spacer.gif" width="1" height="171" alt=""></td>
+	</tr>
+	<tr>
+		<td rowspan="7">
+			<img src="Assets/CastShow_16.gif" width="14" height="491" alt=""></td>
+		<td>
+			<input type="image" name="cast" value="cast" src="Assets/CastShow_17.gif" id"cast"></td>
+		<td rowspan="7">
+			<img src="Assets/CastShow_18.gif" width="16" height="491" alt=""></td>
+		<td>
+			<img src="Assets/spacer.gif" width="1" height="38" alt=""></td>
+	</tr>
+	<tr>
+		<td>
+			<img src="Assets/CastShow_19.gif" width="90" height="111" alt=""></td>
+		<td>
+			<img src="Assets/spacer.gif" width="1" height="111" alt=""></td>
+	</tr>
+	<tr>
+		<td>
+			<input type="image" name="uncast" value="uncast" src="Assets/CastShow_20.gif" id"uncast"></td>
+		<td>
+			<img src="Assets/spacer.gif" width="1" height="37" alt=""></td>
+	</tr>
+	<tr>
+		<td rowspan="4">
+			<img src="Assets/CastShow_21.gif" width="90" height="305" alt=""></td>
+		<td>
+			<img src="Assets/spacer.gif" width="1" height="209" alt=""></td>
+	</tr>
+	<tr>
+		<td rowspan="3">
+			<img src="Assets/CastShow_22.gif" width="410" height="96" alt=""></td>
+		<td colspan="3">
+			<img src="Assets/CastShow_23.gif" width="408" height="34" alt=""></td>
+		<td>
+			<img src="Assets/spacer.gif" width="1" height="34" alt=""></td>
+	</tr>
+	<tr>
+		<td rowspan="2">
+			<img src="Assets/CastShow_24.gif" width="241" height="62" alt=""></td>
+		<td>
+			<input type="image" name="castshow" value="castshow" src="Assets/CastShow_25.gif" id"castshow"></td>
+		<td rowspan="2">
+			<img src="Assets/CastShow_26.gif" width="56" height="62" alt=""></td>
+		<td>
+			<img src="Assets/spacer.gif" width="1" height="37" alt=""></td>
+	</tr>
+	<tr>
+		<td>
+			<img src="Assets/CastShow_27.gif" width="111" height="25" alt=""></td>
+		<td>
+			<img src="Assets/spacer.gif" width="1" height="25" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="11">
+			<img src="Assets/CastShow_28.gif" width="1400" height="31" alt=""></td>
+		<td>
+			<img src="Assets/spacer.gif" width="1" height="31" alt=""></td>
+	</tr>
+</table>
+</form>
+<!-- End Save for Web Slices -->
+</body>
+</html>
---
+++ TermsOfUse.php
@@ -0,0 +1,49 @@
+<html>
+<head>
+<title>AGL: Terms of Use</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+<body bgcolor="#00000" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+<!-- Save for Web Slices (TermsOfUse.psd) -->
+<form name="form" method="post" action="TermsOfUse.php">
+<table width="1400" height="967" border="0" align="center" cellpadding="0" cellspacing="0" id="Table_01">
+	<tr>
+		<td colspan="7">
+			<img src="Assets/TermsOfUse_01.gif" width="1400" height="255" alt=""></td>
+	</tr>
+	<tr>
+		<td rowspan="4">
+			<img src="Assets/TermsOfUse_02.gif" width="384" height="712" alt=""></td>
+		<td width="654" height="564" colspan="5" background="Assets/TermsOfUse_03.gif">&nbsp;
+        <textarea name="previousexperience" id="previousexperience" cols="77" rows="34" style="color: #FFFFFF;border:none;background-color:transparent;" readonly></textarea>
+        </td>
+		<td rowspan="4">
+			<img src="Assets/TermsOfUse_04.gif" width="362" height="712" alt=""></td>
+	</tr>
+	<tr>
+		<td colspan="5">
+			<img src="Assets/TermsOfUse_05.gif" width="654" height="25" alt=""></td>
+	</tr>
+	<tr>
+		<td rowspan="2">
+			<img src="Assets/TermsOfUse_06.gif" width="133" height="123" alt=""></td>
+		<td>
+			<input type="image" name="accept" value="accept" src="Assets/TermsOfUse_07.gif" id="accept"></td>
+		<td rowspan="2">
+			<img src="Assets/TermsOfUse_08.gif" width="200" height="123" alt=""></td>
+		<td>
+			<input type="image" name="decline" value="decline" src="Assets/TermsOfUse_09.gif" id="decline"></td>
+		<td rowspan="2">
+			<img src="Assets/TermsOfUse_10.gif" width="139" height="123" alt=""></td>
+	</tr>
+	<tr>
+		<td>
+			<img src="Assets/TermsOfUse_11.gif" width="91" height="86" alt=""></td>
+		<td>
+			<img src="Assets/TermsOfUse_12.gif" width="91" height="86" alt=""></td>
+	</tr>
+</table>
+</form>
+<!-- End Save for Web Slices -->
+</body>
+</html>
