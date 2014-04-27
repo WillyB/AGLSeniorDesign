9d2e7a3c883dba270ada37f8a650e518b42390c1
WillyB <bateswc@gmail.com>
2014-04-27 11:35:49 -0400

    Baby steps

--- Calendar.js
+++ Calendar.js
@@ -497,6 +497,16 @@ $(document).ready(function(){
 		}	
 	});
     
-    
+    function addGivenAgenda (title, startDate, endDate, boolAllDay,dataArray, colorArray) {
+        jfcalplugin.addAgendaItem(
+	       "#mycal",
+	       title,
+	       startDate,
+	       endDate,
+	       boolAllDay,
+	       dataArray,
+	       colorArray,	
+        );
+    };
 	
 });
