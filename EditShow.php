0d2abba0d84c0ef84b5ecf8292bc547925d57e70
WillyB <bateswc@gmail.com>
2014-04-27 15:45:13 -0400

    Revert "f"
    This reverts commit e23fae2918690821e96d2c1a33a70d3e6cadc163.

--- EditShow.php
+++ EditShow.php
@@ -40,6 +40,10 @@ Include JQuery Core (Required for calendar plugin)
 	$role = $_COOKIE['role'];
 	$email = $_COOKIE['email'];
 	$password = $_COOKIE['password'];
+    
+    //Test variables:
+$startDate = array("2014","03","14","12","0","0","0");
+$endDate = array("2014","03","30","12","0","0","0");
 
 	//No unauthorized access
 	if(!isset($_COOKIE['email']) || !isset($_COOKIE['password']) || !isset($_COOKIE['role']))
@@ -81,6 +85,8 @@ Include JQuery Core (Required for calendar plugin)
 			 "window.location = 'LogIn.php';</script>";//redirect to login page
 		exit;	
 	}
+    
+                
     
 //    if (isset($_POST['save']))
 //    {
