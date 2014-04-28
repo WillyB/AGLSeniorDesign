60bd688367ce71ba6db86d75f32e263581411f3a
WillyB <bateswc@gmail.com>
2014-04-27 21:52:34 -0400

    Yay edits!

--- SaveShowEdit.php
+++ SaveShowEdit.php
@@ -1,5 +1,13 @@
 <?php
-include 'MasterCode.php';
+		$user_name = 'actorsgu_data';
+       	$pass_word = 'cliffy36&winepress';
+		$database = 'actorsgu_data';
+		//$server = 'box293.bluehost.com:3306';
+		$server = 'localhost:3306';
+        
+		$con = mysql_connect($server, $user_name, $pass_word, $database);
+		$db_handle = mysql_connect($server, $user_name, $pass_word);
+		$db_found = mysql_select_db($database, $db_handle);
 //SaveShowEdit
 
 $eventArray = $_REQUEST['laSingleEvent'];
@@ -15,11 +23,14 @@ $backColor = $eventArray[6];
 $foreColor = $eventArray[7];
 $showID    = $eventArray[8];
 
-$db_handle = mysql_connect($server, $user_name, $pass_word);
-$db_found = mysql_select_db($database, $db_handle);
+//$db_handle = mysql_connect($server, $user_name, $pass_word);
+//$db_found = mysql_select_db($database, $db_handle);
                 
+//$SQL = "INSERT INTO Show_Events (Shows_idShows, Title, Start_Date, End_Date, All_Day, First_Name, Last_Name, Background_Color, Foreground_Color) 
+//                         VALUES ($showID, '$title', '$startDate', '$endDate', '$allDay', '$firstName', '$lastName', '$backColor', '$foreColor')";
+
 $SQL = "INSERT INTO Show_Events (Shows_idShows, Title, Start_Date, End_Date, All_Day, First_Name, Last_Name, Background_Color, Foreground_Color) 
-                         VALUES ($showID, '$title', '$startDate', '$endDate', '$allDay', '$firstName', '$lastName', '$backColor', '$foreColor')";
+                         VALUES (1, 'FooBar', 'tomorrow', 'yesterday', 'true', 'Mr', 'RainMan', 'Mauve', 'LimeYellow')";
 $result = mysql_query($SQL);
 $num_rows = mysql_num_rows($result);
 
