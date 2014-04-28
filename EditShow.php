15e1a3871122c9e6ad51e2d00b53b45bfd473aed
WillyB <bateswc@gmail.com>
2014-04-27 20:42:30 -0400

    fixes1

--- EditShow.php
+++ EditShow.php
@@ -937,22 +937,22 @@ var clickAgendaItem = "";
     function saveEvent(){
         var laItems = jfcalplugin.getAllAgendaItems("#mycal");
         laItems.forEach(function(entry) {
-            lsStartDate =   entry['startDate'].toJSON();
-            lsEndDate   =   entry['endDate'].toJSON();
-            lsBacgroundColor    =   entry.displayProp.backgroundColor;
-            lsForegroundColor   =   entry.displayProp.foregroundColor;
-            lsShowID            =   <?php echo $showID?>;
-            lsAllDay            =   entry.allDay.toString();
-            var laSingleEvent   =   [entry.title, lsStartDate, lsEndDate, lsAllDay, entry.data.fname, entry.data.lname, lsBackgroundColor, lsForegroundColor, lsShowID];
+//            lsStartDate =   entry['startDate'].toJSON();
+//            lsEndDate   =   entry['endDate'].toJSON();
+//            lsBacgroundColor    =   entry.displayProp.backgroundColor;
+//            lsForegroundColor   =   entry.displayProp.foregroundColor;
+//            lsShowID            =   < php //echo $showID?>;
+//            lsAllDay            =   entry.allDay.toString();
+//            var laSingleEvent   =   [entry.title, lsStartDate, lsEndDate, lsAllDay, entry.data.fname, entry.data.lname, lsBackgroundColor, lsForegroundColor, lsShowID];
             alert(entry.displayProp.backgroundColor);
-            $.ajax({
-                type:   "POST",
-                url:    "SaveShowEdit.php",
-                data:   { evenData : laSingleEvent },
-                success: function() {
-                    alert('Save successful!');
-                }
-            });
+//            $.ajax({
+//                type:   "POST",
+//                url:    "SaveShowEdit.php",
+//                data:   { evenData : laSingleEvent },
+//                success: function() {
+//                    alert('Save successful!');
+//                }
+//            });
         });
     }
 </script>
