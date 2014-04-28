cf9311f18572e4d4c44cba5ca2ae1c4d4926d2b8
WillyB <bateswc@gmail.com>
2014-04-28 09:36:30 -0400

    Whelp, it's on way to do it

--- ViewShow.php
+++ ViewShow.php
@@ -156,11 +156,63 @@ function limitText(limitField, limitCount, limitNum) {
 			}
 			else
 			{
-				$showtitle = $_POST['title'];
+	$db_handle = mysql_connect($server, $user_name, $pass_word);
+	$db_found = mysql_select_db($database, $db_handle);
+	
+	if ($db_found)
+	{	
+		$SQL = "SELECT * FROM Personnel WHERE Contact_Email = '$email' AND password = '$password'";
+		$result = mysql_query($SQL);
+		$db_field = mysql_fetch_array($result);
+		$num_rows = mysql_num_rows($result);
+		if($num_rows > 0)
+		{
+			$personnelID = $db_field['idPersonnel'];
+			
+			$SQL = "SELECT * FROM Audition WHERE Personnel_idPersonnel = '$personnelID' AND Shows_idShows = '$showID'";
+			$result = mysql_query($SQL);
+			$num_rows = mysql_num_rows($result);
+			if($num_rows > 0)
+			{
+				echo "<script type='text/javascript'>
+		 		 		alert('You have already auditioned for this show!');".
+		 				"window.location = 'ListShows.php';</script>";
+			}
+			else
+			{
+				$SQL = "INSERT INTO Audition (Personnel_idPersonnel, Shows_idShows) VALUES ('$personnelID', '$showID') ";
+				$result = mysql_query($SQL);
+                $auditionID = mysql_insert_id();
+                $showtitle = $_POST['title'];
 				setcookie('showtitle', $showtitle);
 				setcookie('showID', $showID);
+                setcookie('auditionID', $auditionID);
 				echo "<script type='text/javascript'>
+                      alert('Thank you for signing up to audition! On the next page, please list any conflicts you have with the show calendar \(keep in mind you may need to advance a month or two to see all of it\)');
 					  window.location = 'Audition.php';</script>";//redirect to login page
+			}
+		}
+		else
+		{
+		  
+			echo "<script type='text/javascript'>
+				 alert('An error has occured.');".
+				 "window.location = 'ViewShow.php';</script>";//redirect back to Create Show   
+			exit;//exit, so that the following code is not executed
+		}
+				
+		echo "<script type='text/javascript'>
+			  window.location='ViewShow.php';</script>";		
+		exit;	
+    }
+	else//if DB was not found
+	{
+		echo '<script type="text/javascript"> 
+			  alert("Database is not found");
+			  </script>';	
+		exit;
+	}
+				
 				exit;
 			}
 		}
