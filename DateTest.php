0988b2681dabe0beea4732944738ad365f0ad50f
WillyB <bateswc@gmail.com>
2014-05-04 05:49:32 -0400

    Further date tests

--- DateTest.php
+++ DateTest.php
@@ -3,6 +3,10 @@
 $Date = date('Y-m-d H:i:s');
 echo date('Y-m-d H:i:s', $Date);
 $Date = date('Y-m-d H:i:s', strtotime("+1 days"));
+echo "<br/>";
 echo date('Y-m-d H:i:s', $Date);
+echo "<br/>";
+$thisDate = date('Y-m-d H:i:s', time());
+echo date('Y-m-d H:i:s', $thisDate);
 
 ?>
