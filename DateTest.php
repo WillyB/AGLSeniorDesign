e4a283a701e7e5b4922ac8e071d76bd6f16ffa80
WillyB <bateswc@gmail.com>
2014-05-04 05:28:58 -0400

    Date test fix

--- DateTest.php
+++ DateTest.php
@@ -1,8 +1,8 @@
 <?php
 //Date test function
 $Date = date('Y-m-d H:i:s');
-echo ('Y-m-d H:i:s', $Date);
+echo date('Y-m-d H:i:s', $Date);
 $Date = date('Y-m-d H:i:s', strtotime("+1 days"));
-echo ('Y-m-d H:i:s', $Date);
+echo date('Y-m-d H:i:s', $Date);
 
 ?>
