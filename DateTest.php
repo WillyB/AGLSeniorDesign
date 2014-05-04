8e2939e80fe88169c26dfbe7f47e8c3075b4cdc2
WillyB <bateswc@gmail.com>
2014-05-04 05:52:52 -0400

    Yet more date tests...

--- DateTest.php
+++ DateTest.php
@@ -8,5 +8,7 @@ echo date('Y-m-d H:i:s', $Date);
 echo "<br/>";
 $thisDate = date('Y-m-d H:i:s', time());
 echo date('Y-m-d H:i:s', $thisDate);
-
+echo "<br/>";
+$nextDate = date();
+echo date('Y-m-d', $nextDate);
 ?>
