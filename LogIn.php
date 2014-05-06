b73736dbc4fa124a294bd99a4fd8fde9751082c6
gmlinscott <glinscott@uky.edu>
2014-05-05 23:06:33 -0400

    crypt v9

--- LogIn.php
+++ LogIn.php
@@ -135,12 +135,13 @@ if(isset($_COOKIE['email']) && isset($_COOKIE['password']) && isset($_COOKIE['ro
 			//query database with entered data
 			$SQL = "SELECT password FROM Personnel WHERE Contact_Email='$email'";
 			$dbHashedPassword = mysql_query($SQL);
+			$result = mysql_real_escape_string($dbHashedPassword, $db_handle);
 			
 			$num_rows1 = mysql_num_rows($dbHashedPassword);
 			if($num_rows1 > 0)
 			{
-				$cryptresult = crypt($Password, $dbHashedPassword);
-				if ($cryptresult == $dbHashedPassword) 
+				$cryptresult = crypt($Password, $result);
+				if ($cryptresult == $result) 
 				{
 					//authenticated
 					$SQL = "SELECT Role FROM Personnel WHERE Contact_Email='$email' AND BINARY password='$Password'";			
