fa447b4f5d1ba3c85eb76561aaa39fb57a61d561
WillyB <bateswc@gmail.com>
2014-05-06 10:10:16 -0400

    Ajax call test 2

--- ResetPassword.php
+++ ResetPassword.php
@@ -1,5 +1,12 @@
 <?php
+include 'MasterCode.php';
 
+//$con = mysql_connect($server, $user_name, $pass_word, $database);
+$db_handle = mysql_connect($server, $user_name, $pass_word);
+$db_found = mysql_select_db($database, $db_handle);
+
+$newPassword = $_REQUEST['newPassword'];
+$userID      = $_REQUEST['userID'];
 echo '<script type="text/javascript"> 
 			  alert("New password recieved!");
 			  </script>';
