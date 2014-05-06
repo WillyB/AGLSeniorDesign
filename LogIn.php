6aba598fea6cf66e7bca40c448833be5d1bdcc10
gmlinscott <glinscott@uky.edu>
2014-05-06 00:01:42 -0400

    crypt v21

--- LogIn.php
+++ LogIn.php
@@ -136,7 +136,12 @@ if(isset($_COOKIE['email']) && isset($_COOKIE['password']) && isset($_COOKIE['ro
 			$SQL = "SELECT password FROM Personnel WHERE Contact_Email='$email'";
 			$resource = mysql_query($SQL);
 			$dbHashedPassword = mysql_result($resource, 0);
-			echo "<h1>'$dbHashedPassword'</h1>";
+			
+			echo "<script type='text/javascript'>
+				 alert($dbHashedPassword);".
+				 "window.location = 'LogIn.php';</script>";//redirect back to login page   
+			exit;//exit, so that the following code is not executed
+
 			$num_rows1 = mysql_num_rows($resource);
 			if($num_rows1 > 0)
 			{
